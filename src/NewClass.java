3458bcd45f60bb5eeb2fed775ef5b8ff2ac9306c
shasidu <shasidumalshan9579@gmail.com>
2024-09-26 09:00:53 +0530

    six commit

---
+++ src/NewClass.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author shasidu
+ */
+public class NewClass {
+    
+    
+    private String name;
+    private int age;
+    private int batch;
+
+    
+    NewClass(){
+    
+    
+    
+    
+    
+    
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the batch
+     */
+    public int getBatch() {
+        return batch;
+    }
+
+    /**
+     * @param batch the batch to set
+     */
+    public void setBatch(int batch) {
+        this.batch = batch;
+    }
+    
+    
+    
+}
